8864a3d39c289aca6241b4e5f7eb09225ad6bedf
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-08-25 14:16:49 +0700

    w
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- Temperature-reader.py
+++ Temperature-reader.py
@@ -1,13 +1,6 @@
 def temperature_tracker():
-    temperatures = open('hk-temperatures-2024.txt',):
-    
-    if temperatures:
-        avg_temp = sum(temperatures) / len(temperatures)
-        max_temp = max(temperatures)
-        min_temp = min(temperatures)
-        
-        print(f"Average Temperature: {avg_temp:.2f}")
-        print(f"Maximum Temperature: {max_temp:.2f}")
-        print(f"Minimum Temperature: {min_temp:.2f}")
-    else:
-        print("No temperatures were entered.")
+    with open('hk-temperatures-2024.txt', 'r') as file:
+        rounded_temp = round(float(temperatures.strip()))
+        for temperatures in file:
+            
+temperature_tracker()
