e7921858c6502fcb6548ced7dda2827c4e6fa2be
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-08-26 12:32:02 +0700

    c
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- Temperature-reader.py
+++ Temperature-reader.py
@@ -1,6 +1,19 @@
 def temperature_tracker():
     with open('hk-temperatures-2024.txt', 'r') as file:
-        rounded_temp = round(float(temperatures.strip()))
-        for temperatures in file:
+        readtemp = file.readlines()
+        for readtemp in file:
+            Temp = float(readtemp.strip())
+            templist.append(Temp)
+        for Temp in templist:
+            roundtemp = round(Temp)
+            roundlist.append(roundtemp)
+
             
+templist = []
+roundlist = []
+tot_temp = 0
+Temp = 0
+max_temp = -100
+min_temp = 100
 temperature_tracker()
+print (roundlist)
