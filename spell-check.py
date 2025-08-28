b5b39f99fde7b941da0d25c14ecbf28b40d8c067
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-08-28 08:52:07 +0700

    Update spell-check.py
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- spell-check.py
+++ spell-check.py
@@ -1,7 +1,11 @@
+#Define a function to check for misspelled words
 def spell_check():
+    #Declare global variable to count misspelled words
     global misspelled
     correct_words = []
     mystery_words = []
+    #Call out to a dictionary file and a mystery text file
+    #Read the files and store the words in two separate lists
     with open("dictionary.txt", "r") as file:
         text = file.readlines()
         for word in text:
@@ -10,11 +14,13 @@ def spell_check():
         text = file.read()
         for word in text.split():
             mystery_words.append(word.strip().lower())
+    #Compare the two lists and count how many words are misspelled in integers
     for word in mystery_words:
         if word not in correct_words:
             misspelled += 1
 
-
+#Initialize global variable
 misspelled = 0
+#Run the spell check function and print the number of misspelled words
 spell_check()
 print(f"The number of misspelled words is: {misspelled}")
