cd84c772e4165f0a1b48605aca3f294846e28f8d
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-08-28 08:28:46 +0700

    Update Temperature-reader.py
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- Temperature-reader.py
+++ Temperature-reader.py
@@ -1,13 +1,15 @@
+# Import necessary library (time)
 import time
-
+# Define function to read temperatures from file and calculate statistics
 def temperature_tracker():
+    # Declare global variables, so they can be modified inside the function
     global tot_temp
     global avg_temp
     global max_temp
     global min_temp
     templist = []
     roundlist = []
-    
+    # Read temperatures from file and process them through temporary list into rounded list
     with open('hk-temperatures-2024.txt', 'r') as file:
         readtemp = file.readlines()
         for line in readtemp:
@@ -16,17 +18,17 @@ def temperature_tracker():
         for Temp in templist:
             roundtemp = round(Temp)
             roundlist.append(roundtemp)
-    
+    # Calculate total, average, maximum, and minimum temperatures
     tot_temp = sum(roundlist)
     avg_temp = tot_temp / len(roundlist)
     max_temp = max(roundlist)
     min_temp = min(roundlist)
-
+# Initialize global variables
 tot_temp = 0
 avg_temp = 0
 max_temp = -100
 min_temp = 100
-
+# Run the temperature tracker function every 24 hours (86400 seconds)
 while True:
     temperature_tracker()
     print(f"The total temperature is: {tot_temp}")
