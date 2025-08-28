844e907aeb321a35697d1912e110fe0bdb577e59
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-08-28 08:45:08 +0700

    second challenge complete i think
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- spell-check.py
+++ spell-check.py
@@ -1,6 +1,20 @@
 def spell_check():
+    global misspelled
+    correct_words = []
+    mystery_words = []
     with open("dictionary.txt", "r") as file:
-        words = file.readline()
-    print(words)
+        text = file.readlines()
+        for word in text:
+            correct_words.append(word.strip().lower())
+    with open("mystery-text.txt", "r") as file:
+        text = file.read()
+        for word in text.split():
+            mystery_words.append(word.strip().lower())
+    for word in mystery_words:
+        if word not in correct_words:
+            misspelled = misspelled + 1
 
+
+misspelled = 0
 spell_check()
+print(f"The number of misspelled words is: {misspelled}")
