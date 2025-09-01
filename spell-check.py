859d53b24e0b671d384e392967d18d0d61b72efe
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-09-01 11:10:04 +0700

    added file handling error for challenege 2
    file not found
    
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- spell-check.py
+++ spell-check.py
@@ -5,19 +5,24 @@ def spell_check():
     correct_words = []
     mystery_words = []
     #Call out to a dictionary file and a mystery text file
-    #Read the files and store the words in two separate lists
-    with open("dictionary.txt", "r") as file:
-        text = file.readlines()
-        for word in text:   
-            correct_words.append(word.strip().lower())
-    with open("mystery-text.txt", "r") as file:
-        text = file.read()
-        for word in text.split():
-            mystery_words.append(word.strip().lower())
-    #Compare the two lists and count how many words are misspelled in integers
-    for word in mystery_words:
-        if word not in correct_words:
-            misspelled += 1
+    try:
+        # Read the dictionary file and store words in a list
+        with open("dictionary.txt", "r") as file:
+            text = file.readlines()
+            for word in text:
+                correct_words.append(word.strip().lower())
+        # Read the mystery text file and store words in a list
+        with open("mystery-text.txt", "r") as file:
+            text = file.read()
+            for word in text.split():
+                mystery_words.append(word.strip().lower())
+        #Compare the two lists and count how many words are misspelled in integers
+        for word in mystery_words:
+            if word not in correct_words:
+                misspelled += 1
+    # Add exception handling for file not found
+    except FileNotFoundError:
+        print("Error: One of the files is not found.")
 
 #Initialize global variable
 misspelled = 0
