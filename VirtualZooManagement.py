edf5cce9d768047cbc9f2596db10211e5e8452b3
EgorKos-dev <egorkos@bisphuket.ac.th>
2025-09-05 14:37:40 +0700

    more than a half complete

--- VirtualZooManagement.py
+++ VirtualZooManagement.py
@@ -1,43 +1,50 @@
-# Import necessary modules
+
 import random
-# Define the function to ask the user to add a new animal to the zoo
-def virtual_zoo_management():
+counter = 10
+
+def menu():
+    animal_options = input("1. Add a new animal to the zoo\n2. View all the animals\n3. Zoo summary\nChoose an option (1-3): ")
+    if animal_options.lower() == '1':
+        add_animalfunc()
+
+def add_animalfunc():
     global counter
-    animalname = []
-    animalspecies = []
-    animalage = []
-    animalweight = []
     print("Welcome to the Virtual Zoo Management System!")
-    for i in range(counter):
+    while counter > 0:
         new_animal = input("Enter the name of the animal, species, age and weight you want to add (or type 'exit' to quit): ")
         if new_animal.lower() == 'exit':
-            print("Exiting the Virtual Zoo Management System. Goodbye!")
+            print("Exiting the System. Goodbye!")
+            menu()
             break
         elif new_animal:
-            for i in range(4):
-                if i == 0:
-                    animalname = new_animal.split()[0]
-                elif i == 1:
-                    animalspecies = new_animal.split()[1]
-                elif i == 2:
-                    if int(new_animal.split()[2]) < 0 or int(new_animal.split()[2]) > 50:
-                        print("Error: Age must be in range of 1-50. Please re-enter the details.")
-                        return virtual_zoo_management()
-                    animalage = new_animal.split()[2]
-                elif i == 3:
-                        if int(new_animal.split()[3]) < 0 or int(new_animal.split()[3]) > 1000:
-                            print("Error: Weight must be in range of 0.1-1000. Please re-enter the details.")
-                            return virtual_zoo_management()
-                        animalweight = new_animal.split()[3]
-            print(f"Animal added: Name: {animalname}, Species: {animalspecies}, Age: {animalage}, Weight: {animalweight}kg. Is that correct?")
+            parts = new_animal.split()
+            if len(parts) != 4:
+                print("Error: Please enter exactly 4 values (name, species, age, weight).")
+                continue
+            animalname, animalspecies, animalage, animalweight = parts
+            try:
+                age = int(animalage)
+                weight = float(animalweight)
+            except ValueError:
+                print("Error: Age must be an integer and weight must be a real number.")
+                continue
+            if age < 1 or age > 50:
+                print("Error: Age must be in range of 1-50. Please re-enter the details.")
+                continue
+            if weight < 0.1 or weight > 1000:
+                print("Error: Weight must be in range of 0.1-1000. Please re-enter the details.")
+                continue
+            print(f"Animal added: Name: {animalname}, Species: {animalspecies}, Age: {age}, Weight: {weight}kg. Is that correct?")
             confirmation = input("Type 'yes' to confirm or 'no' to re-enter the details: ")
             if confirmation.lower() == 'yes':
+                with open("Animals.txt", "a") as add_animal:
+                    add_animal.write(f"{[animalname, animalspecies, age, weight]}\n")
                 print(f"Successfully added {animalname} the {animalspecies} to the zoo!")
+                counter -= 1
             else:
-                virtual_zoo_management()   
-        counter -= 1
+                continue
+    if counter == 0:
+        print("You have reached the maximum number of animals you can add.")
+        menu()
 
-counter = 10
-zoo_management = input("1. Add a new animal to the zoo\n2. View all the animals\n3. Zoo summary\nChoose an option (1-3): ")
-if add_animal.lower() == 'yes':
-    virtual_zoo_management()
+menu()
